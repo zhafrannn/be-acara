352e28d2d33963973a8ed4ce63f09ad0142d55f0
zhafrannn <zhafran858@gmail.com>
2025-10-10 17:59:04 +0700

    fix: typo

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -2,62 +2,82 @@ import swaggerAutogen from "swagger-autogen";
 
 // ini adalah konfigurasi dari swagger nya
 const doc = {
-    info: {
-        version: "v0.0.1",
-        title: "API Documentation Event",
-        description: "API Documentation Event",
+  info: {
+    version: "v0.0.1",
+    title: "API Documentation Event",
+    description: "API Documentation Event",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Server",
     },
-    servers: [
-        {
-            url: "http://localhost:3000/api",
-            description: "Local Server",
-        },
-        {
-            url: "https://be-acara-theta.vercel.app/api",
-            description: "Deploy Server",
-        },
-    ],
-    components: {
-        securitySchemes : {
-            bearerAuth: {
-                type: "http",
-                scheme: "bearer",
-            },
-        },
-        schemas: {
-            LoginRequest: {
-                identifier: "sungjinwoo",
-                password: "Asdqwe123",
-            },
-            RegisterRequest: {
-                fullName: "Sung Jin Woo",
-                username: "sungjinwoo",
-                email: "sjw2024@yopmail.com",
-                password: "Asdqwe123",
-                confirmPassword: "Asdqwe123",
-
-            },
-            ActivationRequest: {
-                code: "abcdef",
-            }
-        },
+    {
+      url: "https://be-acara-theta.vercel.app/api",
+      description: "Deploy Server",
     },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "sjw2025@yopmail.com",
+        password: "Asdqwe123",
+      },
+      RegisterRequest: {
+        fullName: "Sung Jin Woo",
+        username: "sungjinwoo",
+        email: "sjw2024@yopmail.com",
+        password: "Asdqwe123",
+        confirmPassword: "Asdqwe123",
+      },
+      ActivationRequest: {
+        code: "abcdef",
+      },
+      CreateCategoryRequest: {
+        name: "",
+        description: "",
+        icon: "",
+      },
+      CreateEventRequest: {
+        name: "",
+        banner: "fileUrl",
+        category: "category ObjectID",
+        description: "",
+        startDate: "yyyy-mm-dd hh:mm:ss",
+        endDate: "yyyy-mm-dd hh:mm:ss",
+        location: {
+          region: "region id",
+          coordinates: [0, 0],
+        },
+        isOnline: false,
+        isFeatured: false,
+      },
+      RemoveMediaRequest: {
+        fileUrl: "",
+      },
+    },
+  },
 };
 
-/** 
+/**
  * ouputFile ini adalah file yang nanti akan dibaca oleh swagger
  * sifatnya generated tidak perlu diubah secara manual
- * */ 
+ * */
 const outputFile = "./swagger_output.json";
 
 // untuk membaca ada endpoint apa saja di api.ts
 const endpointsFiles = ["../routes/api.ts"];
 
-swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc)
-
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
 
 /**
  * ketika dijalankan npm run docs
  * maka akan menjalankan docs yang telah diatur sebelumnya di package.json yang mengarah ke file ini yang dimana adalah konfigurasi swagger
  * lalu setelah itu akan muncul file bernama swagger_output.json
- */ 
+ */
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -23,18 +23,62 @@ router.post(
   "/category",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
   categoryController.create
+  /*
+  #swagger.tags = ['Category']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    schema: {
+      $ref: "#/components/schemas/CreateCategoryRequest"
+    }
+  }
+  */
 );
-router.get("/category", categoryController.findAll);
-router.get("/category/:id", categoryController.findOne);
+router.get(
+  "/category",
+  categoryController.findAll
+  /* 
+  #swagger.tags = ['Category'] 
+  */
+);
+router.get(
+  "/category/:id",
+  categoryController.findOne
+  /* 
+  #swagger.tags = ['Category'] 
+  */
+);
 router.put(
   "/category/:id",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
   categoryController.update
+  /* 
+  #swagger.tags = ['Category']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    schema: {
+      $ref: "#/components/schemas/CreateCategoryRequest"
+    }
+  } 
+  */
 );
 router.delete(
   "/category/:id",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
   categoryController.remove
+  /* 
+  #swagger.tags = ['Category']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+  */
 );
 
 // Event
@@ -42,28 +86,114 @@ router.post(
   "/events",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
   eventController.create
+  /*
+  #swagger.tags = ['Events']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    schema: {
+      $ref: "#/components/schemas/CreateEventRequest"
+    }
+  }
+  */
 );
-router.get("/events", eventController.findAll);
-router.get("/events/:id", eventController.findOne);
+router.get(
+  "/events",
+  eventController.findAll
+  /*
+  #swagger.tags = ['Events']
+  */
+);
+router.get(
+  "/events/:id",
+  eventController.findOne
+  /*
+  #swagger.tags = ['Events']
+  */
+);
 router.put(
   "/events/:id",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
   eventController.update
+  /*
+  #swagger.tags = ['Events']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    schema: {
+      $ref: "#/components/schemas/CreateEventRequest"
+    }
+  }
+  */
 );
 router.delete(
   "/events/:id",
   [authMiddleware, aclMiddleware([ROLES.ADMIN])],
   eventController.remove
+  /*
+  #swagger.tags = ['Events']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+  */
 );
-router.get("/events/:slug/slug", eventController.findOneBySlug);
+router.get(
+  "/events/:slug/slug",
+  eventController.findOneBySlug
+  /*
+  #swagger.tags = ['Events']
+  */
+);
 
 // Region
-router.get("/regions", regionController.getAllProvinces);
-router.get("/regions/:id/province", regionController.getProvince);
-router.get("/regions/:id/regency", regionController.getRegency);
-router.get("/regions/:id/district", regionController.getDistrict);
-router.get("/regions/:id/village", regionController.getVillage);
-router.get("/regions-search", regionController.findByCity);
+router.get(
+  "/regions",
+  regionController.getAllProvinces
+  /*
+  #swagger.tags = ['Regions']
+  */
+);
+router.get(
+  "/regions/:id/province",
+  regionController.getProvince
+  /*
+  #swagger.tags = ['Regions']
+  */
+);
+router.get(
+  "/regions/:id/regency",
+  regionController.getRegency
+  /*
+  #swagger.tags = ['Regions']
+  */
+);
+router.get(
+  "/regions/:id/district",
+  regionController.getDistrict
+  /*
+  #swagger.tags = ['Regions']
+  */
+);
+router.get(
+  "/regions/:id/village",
+  regionController.getVillage
+  /*
+  #swagger.tags = ['Regions']
+  */
+);
+router.get(
+  "/regions-search",
+  regionController.findByCity
+  /*
+  #swagger.tags = ['Regions']
+  */
+);
 
 // Media
 router.post(
@@ -74,6 +204,32 @@ router.post(
     mediaMiddleware.single("file"),
   ],
   mediaController.single
+  /*
+  #swagger.tags = ['Media']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    content: {
+      "multipart/form-data": {
+         schema: {
+          type: "object",
+          properties: {
+            file: {
+              type: "string",
+              format: "binary",
+            }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+  }
+  */
 );
 router.post(
   "/media/upload-multiple",
@@ -83,11 +239,53 @@ router.post(
     mediaMiddleware.multiple("files"),
   ],
   mediaController.multiple
+  /*
+  #swagger.tags = ['Media']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    content: {
+      "multipart/form-data": {
+         schema: {
+          type: "object",
+          properties: {
+            files: {
+              type: "array",
+              items: {
+                type: "string",
+                format: "binary",
+              },
+            }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+  }
+  */
 );
 router.delete(
   "/media/remove",
   [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
   mediaController.remove
+  /*
+  #swagger.tags = ['Media']
+  #swagger.security = [{
+    "bearerAuth": {}
+  }]
+
+  #swagger.requestBody = {
+    required: true,
+    schema: {
+      $ref: "#/components/schemas/RemoveMediaRequest"
+    }
+  }
+  */
 );
 
 export default router;
